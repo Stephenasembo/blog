dec5c3bb87a5e32c476116a3308c1867def6520e
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-11-30 22:04:45 +0300

    Chore: Switch To A Production Database

--- frontend/src/sanity/client.js
+++ frontend/src/sanity/client.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const client = createClient({
   projectId: "44wpwbl4",
-  dataset: "development",
+  dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
 });
