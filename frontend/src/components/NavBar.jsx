dd912ea004b197d291ce342b4fe2a6caf6b6a26a
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-12-01 16:28:53 +0300

    Chore: Change Website Heading

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,11 @@
-import { MenuIcon } from "./Svg";
-
 function NavBar() {
   return (
     <div
-    className="flex justify-between border-b border-gray-300 pb-1 mb-8"
+    className="text-center p-1 border-b border-gray-300 pb-1 mb-8"
     >
       <h1
       className="text-2xl font-bold"
-      >Stephen</h1>
-      <MenuIcon />
+      >Asembo Codex</h1>
     </div>
   )
 }
